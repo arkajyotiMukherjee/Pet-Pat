3e08711289601554837b285a8432e449523ab7a0
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-03-24 21:49:06 +0530

    udated app with shop page, collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const CatPage = () => (
-	<div>
-		<h1>CATS</h1>
-	</div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop/cats' component={CatPage} />
+				<Route exact path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
-	);
+	); 
 }
 
 export default App;
