d0db7b9a7b879828a04006f6ad1e115a3c68c8ad
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-04-07 14:32:00 +0530

    store and authenticate users in firebase db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
 	constructor() {
@@ -20,10 +20,23 @@ class App extends React.Component {
 	unsubcribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-			console.log(user);
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+					console.log(this.state)
+				});
+
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
@@ -34,7 +47,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser}/>
+				<Header currentUser={this.state.currentUser} />
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route exact path='/shop' component={ShopPage} />
