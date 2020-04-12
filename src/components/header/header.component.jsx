6e72392182ba28ef093eb5ca052084f89b657735
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-04-12 20:55:43 +0530

    added carticon and cartdropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/4.4 crown.svg.svg";
 
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className='header'>
 		<Link className='logo-container' to='/'>
 			<Logo className='logo' />
@@ -29,12 +31,15 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			)}
+			<CartIcon />
 		</div>
+		{hidden ? null : <CartDropdown />}
 	</div>
 );
 
-const mapStatetoProps = state => ({
-	currentUser: state.user.currentUser
-})
+const mapStatetoProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
+});
 
 export default connect(mapStatetoProps)(Header);
